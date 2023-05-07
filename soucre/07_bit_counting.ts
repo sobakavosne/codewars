a31b89fe77e57ecda9bace45cafbf6971eff307c
lenchevskii <dima.lenchevskii@gmail.com>
2023-05-07 23:16:26 +0400

    add bit counting

---
+++ soucre/07_bit_counting.ts
@@ -0,0 +1,13 @@
+// Write a function that takes an integer as input, and returns the number 
+// of bits that are equal to one in the binary representation of that number. 
+// You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function 
+// should return 5 in this case
+
+const countBits =
+  (a: number) => a
+    .toString(2)
+    .split("")
+    .filter((digit) => digit === "1")
+    .length;
